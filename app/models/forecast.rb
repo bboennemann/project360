a63fa7eda42cbf725a393c9cda5751cf44debf86
Bernhard Boennemann <bmail@boenne.com>
2016-11-18 11:31:58 -0500

    established embed relationship between forecast and time entry

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -4,6 +4,9 @@ class Forecast
   belongs_to :project
   field :project_id, type: BSON::ObjectId
 
+  embeds_many :time_entries
+  field :time_entry_id, type: BSON::ObjectId
+
 
   field :name, type: String
   field :revision, type: Integer
--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,5 +1,8 @@
 class TimeEntry
   include Mongoid::Document
+
+  embedded_in :forecast
+
   field :entry_date, type: Date
   field :hours, type: Float
 end
