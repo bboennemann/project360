45480a54e9c798034d32e3f8277aef0a12e39d15
Bernhard Boennemann <bmail@boenne.com>
2016-11-22 17:03:03 -0500

    started forecast ajax

--- app/assets/javascripts/forecast.js
+++ app/assets/javascripts/forecast.js
@@ -1,8 +1,22 @@
+function updateForecast(timeEntry){
+    $.ajax({
+        url : '/user_forecasts/' + timeEntry.data('user') ,
+        data : { user_forecast: { hours: timeEntry }},
+        type : 'put',
+        error : function(jqXHR, textStatus, errorThrown) {
+            alert('There was a problem loading the requested content. Please try again later');
+        },
+        success : function(html, resultText) {
+            alert('success');
+        }
+    });
+}
 
 $(document).ready(function() {
 
 	$('.forecast_entry').change(function(){
-		alert(this.value);
+		alert($(this).data("user"));
+		updateForecast(this);
 	});
 
 });
